57b3064b81b36a48ec9a31a1beb94650de709ed5
Lotem Levinzon <lotem.levinzon@post.runi.ac.il>
2025-11-12 01:01:39 +0200

    Completed assignment

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,22 @@
 //feedback
 
-/*
-Great work boaz!
-*/
-
-
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+        String cheer = args[0];
+        String cheerUpperCase = args[0].toUpperCase();
+        String anLetters = "AEFHILMNLORSX";
+	int numOfTimes = Integer.parseInt(args[1]); 
+        for (int i = 0; i < cheerUpperCase.length(); i++) {
+                char letter = cheerUpperCase.charAt(i);
+                if(anLetters.indexOf(letter) == -1) {
+                        System.out.println("Give me a " + cheerUpperCase.charAt(i) + ": " + cheerUpperCase.charAt(i) +"!");
+                } else {
+                        System.out.println("Give me an " + cheerUpperCase.charAt(i) + ": " + cheerUpperCase.charAt(i) +"!");
+                }
+        } System.out.println("What does that spell?");
+        for (int i = 0; i < numOfTimes; i++) {
+                System.out.println(cheerUpperCase + "!!!");
+        }
         }
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,27 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+        int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int minToAdd = Integer.parseInt(args[1]);
+        //System.out.println(hours);
+        //System.out.println(minutes);
+        //System.out.println(minToAdd);
+        int totalMinutes = (hours * 60) + minutes + minToAdd;
+        int totalHours = totalMinutes / 60;
+        int newHours = totalHours % 24;
+        int newMinutes = totalMinutes - (totalHours * 60);
+        //System.out.println(newHours);
+        //System.out.println(newMinutes);
+        if (hours >= 0 && hours < 10 || newHours >= 0 && newHours < 10) {
+System.out.print("0" + newHours);
+        } else if (hours >= 10 && hours < 24 || newHours >=10 && newHours < 24) {
+            System.out.print(newHours);
+        }
+        System.out.print(":"); 
+        if (minutes >= 0 && minutes < 10 || newMinutes >= 0 && newMinutes < 10) {
+            System.out.print("0" + newMinutes);
+        } else {
+            System.out.print(newMinutes);
+        }
     }
 }

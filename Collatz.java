4f4829a09fc8bee57e61d7296f18d24c10328168
Lotem Levinzon <lotem.levinzon@post.runi.ac.il>
2025-11-12 21:56:51 +0200

    Completed assignment

--- Collatz.java
+++ Collatz.java
@@ -2,13 +2,30 @@
 public class Collatz {
 	public static void main(String args[]) {
 	    int seed = Integer.parseInt(args[0]);
-		char mode = args[1];
-		int i = 1;
-		while ( i < seed) {
-			if (seed % 2 == 0) {
-				
+		String mode = args[1];
+		if (mode.equals("v")) {
+		for (int i = 1; i <= seed; i++) {
+			int count = 0;
+			int curNum = i;
+			System.out.print(curNum + " ");
+		while ( i <= seed) {
+			if (curNum % 2 == 0) {
+				curNum = curNum / 2;
+			} else {
+				curNum = curNum * 3 + 1;
 			}
-		}
+			System.out.print(curNum + " ");
+			count++;
+			if (curNum == 1) {
+				break;
+			}
+		} System.out.println("(" + (count + 1) + ")");
+	}  System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
+	} else if (mode.equals("c")) {
+		System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
+	}
+
+	
 
 		
 	}
